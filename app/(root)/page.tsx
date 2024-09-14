48c43fd1c9bc8ded9e73f5f07670994853c67a96
Tanmay Sarin <tanmaysarin@gmail.com>
2024-09-13 22:53:15 -0700

    implement complete homepage layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,13 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = {firstName: 'Tanmay'};
+  const loggedIn = {
+    firstName: 'Tanmay',
+    lastName: 'Sarin',
+    email: 'tanmaysarin@gmail.com'};
 
   return (
     <section className='home'>
@@ -24,6 +28,12 @@ const Home = () => {
         </header>
         RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 123.50}, {currentBalance: 500.00}]}
+      />
     </section>
   )
 }
